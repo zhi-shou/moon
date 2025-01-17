52af7a99207a1224bf09f68a4564c66428993c81
heshouzhi <heshouzhi@millionlab.cn>
2025-01-17 16:55:12 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/moon/',
-  build: {
-    outDir: 'docs'
-  },
   server: {
     host: '0.0.0.0',
     port: 5173,
     https: false
+  },
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+    // 确保生成 JavaScript 文件
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
   }
 })
